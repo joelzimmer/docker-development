c62efe2823eacb02a42367b2aa36b09894832256
Jamie McClelland <jm@mayfirst.org>
2015-10-14 14:09:47 -0400

    ensure we have a separate produce db as well.

--- init.sql
+++ init.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS web;
 CREATE DATABASE IF NOT EXISTS api;
 CREATE DATABASE IF NOT EXISTS memservices;
+CREATE DATABASE IF NOT EXISTS produce;
 GRANT ALL ON web.* TO 'web'@'localhost' identified by 'web';
 GRANT ALL ON api.* TO 'api'@'localhost' identified by 'api';
 GRANT ALL ON memservices.* TO 'api'@'localhost';
+GRANT ALL ON produce.* TO 'api'@'localhost';
